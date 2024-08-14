72d26a75f81443b613abd0f339bac12cefdbe020
Augusto Gangui <61172521+SrBigotones@users.noreply.github.com>
2024-08-14 13:33:38 -0300

    project init

---
+++ src/main/java/com/uade/api/ecommerce/ecommerce/controllers/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.uade.api.ecommerce.ecommerce.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class HelloWorldController {
+
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World!";
+    }
+}
