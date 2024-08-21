2d8cf22eeddf3c0f21fb256ab9d41e56a97ce377
Mateo Nahuel Caré <mcare@uade.edu.ar>
2024-08-20 22:20:05 -0300

    Clase 20/08
    Se agrega un PostMapping que tira error 403

--- src/main/java/com/uade/api/ecommerce/ecommerce/controllers/HelloWorldController.java
+++ src/main/java/com/uade/api/ecommerce/ecommerce/controllers/HelloWorldController.java
@@ -2,11 +2,12 @@ package com.uade.api.ecommerce.ecommerce.controllers;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping ("/API")
 public class HelloWorldController {
 
 
@@ -14,4 +15,7 @@ public class HelloWorldController {
     public String hello(){
         return "Hello World!";
     }
+
+    @PostMapping ("/hola")
+    public String hola() { return "Hola mundo!"; }
 }
