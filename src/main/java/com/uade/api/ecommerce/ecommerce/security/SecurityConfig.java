51c850c4d941a9bdbb5e9af4489d617c692e261e
Augusto Gangui <61172521+SrBigotones@users.noreply.github.com>
2024-10-28 20:11:03 -0300

    cors

--- src/main/java/com/uade/api/ecommerce/ecommerce/security/SecurityConfig.java
+++ src/main/java/com/uade/api/ecommerce/ecommerce/security/SecurityConfig.java
@@ -4,6 +4,7 @@ package com.uade.api.ecommerce.ecommerce.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -13,6 +14,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -29,15 +36,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain firstFilter(HttpSecurity httpSecurity) throws Exception {
 
-        httpSecurity.addFilterBefore(
-                jwtTokenFilter,
-                UsernamePasswordAuthenticationFilter.class
-        );
-
-        httpSecurity.addFilterBefore(
-                adminRouteFilter,
-                UsernamePasswordAuthenticationFilter.class
-        );
+        System.out.println("New request");
 
         httpSecurity
                 .authorizeHttpRequests(auth ->
@@ -47,7 +46,18 @@ public class SecurityConfig {
                                 .anyRequest().authenticated()
                 )
                 .csrf(crsf -> crsf.disable())
+                .cors(c -> c.configurationSource(corsConfigurationSource()))
                 .headers(head -> head.frameOptions(frame -> frame.disable()));
+
+        httpSecurity.addFilterBefore(
+                jwtTokenFilter,
+                UsernamePasswordAuthenticationFilter.class
+        );
+
+        httpSecurity.addFilterBefore(
+                adminRouteFilter,
+                UsernamePasswordAuthenticationFilter.class
+        );
 
         return httpSecurity.build();
     }
@@ -60,6 +70,20 @@ public class SecurityConfig {
         provider.setPasswordEncoder(passwordEncoder());
 
         return new ProviderManager(provider);
+    }
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        //configuration.setAllowedOrigins(List.of("http://localhost:5173/login"));
+        //configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.applyPermitDefaultValues();
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
